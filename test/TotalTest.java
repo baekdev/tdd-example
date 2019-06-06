383273b56d3be7f228128b4ccf980b8f06f8086d
divetech <diveintotechnology@gmail.com>
2019-06-06 21:07:47 +0900

    4장. 프라이버시

--- test/TotalTest.java
+++ test/TotalTest.java
@@ -8,10 +8,13 @@ public class TotalTest extends TestCase {
 
 	public void test_multiplication() {
 		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		assertEquals(10, product.amount);
-		product = five.times(3);
-		assertEquals(15, product.amount);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+
+	public void test_equality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 
 }
