8f1cbbd398ccea1f047b6fe9bdaa8382db5b3e5f
divetech <diveintotechnology@gmail.com>
2019-06-06 20:57:46 +0900

    2장. 타락학 객체

---
+++ src/Dollar.java
@@ -0,0 +1,17 @@
+/**
+ * @author Kayla
+ * @since 2019-06-06
+ */
+
+public class Dollar {
+
+	int amount;
+
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+}
---
+++ test/TotalTest.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+
+/**
+ * @author Kayla
+ * @since 2019-06-06
+ */
+public class TotalTest extends TestCase {
+
+	public void test_multiplication() {
+		Dollar five = new Dollar(5);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
+	}
+
+}
